e4a619bba88c213f4f3a824c015f3efe25a2241f
Timothy Peck <timothy.peck@he-arc.ch>
2023-01-05 14:16:55 +0100

    Update spring version to 3.0.1

--- src/main/java/ch/hearc/tvdb/repertoire/controllers/IndexController.java
+++ src/main/java/ch/hearc/tvdb/repertoire/controllers/IndexController.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
